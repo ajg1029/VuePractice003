805c5272c213d78ab54db3e74ac1b47b7af177ef
Deon <seonbihanbin@gmail.com>
2023-06-08 18:01:44 +0900

    JSPM

--- hello-store-2/src/views/Print.vue
+++ hello-store-2/src/views/Print.vue
@@ -22,22 +22,40 @@ export default {
     // console.log(this)
   },
   methods: {
-    testJSPM() {
-      // return new Promise((resolve, reject) => {
-      //   JSPM.JSPrintManager.auto_reconnect = true
-      //   // JSPM.JSPrintManager.start().then((data) => {
-      //   //   console.log(data)
-      //   //   resolve(data)
-      //   // }).catch((err) => {
-      //   //   console.log(err)
-      //   //   reject(err)
-      //   // })
+    // testJSPM() {
+    //   // return new Promise((resolve, reject) => {
+    //   //   JSPM.JSPrintManager.auto_reconnect = true
+    //   //   // JSPM.JSPrintManager.start().then((data) => {
+    //   //   //   console.log(data)
+    //   //   //   resolve(data)
+    //   //   // }).catch((err) => {
+    //   //   //   console.log(err)
+    //   //   //   reject(err)
+    //   //   // })
 
-      //   // JSPM.JSPrintManager.start().then(function() {
-      //   //   this.cpj = new JSPM.ClientPrintJob()
-      //   // })
-      // })
-      
+    //   //   // JSPM.JSPrintManager.start().then(function() {
+    //   //   //   this.cpj = new JSPM.ClientPrintJob()
+    //   //   // })
+    //   // })
+    //   var cpj = {}
+    //   cpj.clientPrinter = new JSPM.SerialPortPrinter(
+    //     'Brother QL-700',
+    //     9600,
+    //     // JSPM.serial.Parity['None'],        
+    //     // JSPM.serial.StopBits['None'],      
+    //     // JSPM.serial.DataBits['Eight'],      
+    //     // JSPM.serial.Handshake['None'],        
+    //   )
+    //   cpj.sendToClient()
+    // }
+
+    connectPrinter() {
+      // 인쇄 테스트
+      // 아마도 아래의 순서로 작업해보면 될 것 같다.
+      // 1. 프린터 연결
+      // 2. 프린터 설정 (port, baud rate 등)
+      // 3. 몰?루
+
     }
   }
 }
